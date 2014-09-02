65bdd52606718682335d4c5098ea59338fdfa7ed
dakotasanchez <dakotasanchez@outlook.com>
2014-09-01 23:37:47 -0700

    Refactored functions

--- app/src/main/java/com/sanchez/geoopposite/OtherLocationFragment.java
+++ app/src/main/java/com/sanchez/geoopposite/OtherLocationFragment.java
@@ -14,9 +14,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.app.NavUtils;
-import android.text.Editable;
 import android.text.InputType;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -57,17 +55,13 @@ public class OtherLocationFragment extends Fragment {
     // Possibly unsafe to store in var for future
     private static final String API_KEY = "AIzaSyB2Q4JgxW7hLb88fU-IKkxle-J-uxcq_0g";
 
-    private final String TITLE_ACTION_BAR = "Please enter alternate location";
-    private final String NOTHING_ENTERED = "No location or coordinates entered";
-    private final String FILL_BOTH_FIELDS = "Please fill both coordinate fields";
-
     private AutoCompleteTextView cityACTextView;
     private EditText latEditText;
     private EditText longEditText;
-    private Button enterButton;
+    private Button enterButton1;
+    private Button enterButton2;
 
-    public OtherLocationFragment() {
-    }
+    public OtherLocationFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -88,19 +82,18 @@ public class OtherLocationFragment extends Fragment {
             getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        getActivity().getActionBar().setTitle(TITLE_ACTION_BAR);
+        getActivity().getActionBar().setTitle(getResources().getString(R.string.title_action_bar));
 
         setHasOptionsMenu(true);
 
         cityACTextView = (AutoCompleteTextView)rootView.findViewById(R.id.city_AC_textview);
         cityACTextView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
         cityACTextView.setAdapter(new PlacesAutoCompleteAdapter(getActivity(), R.layout.list_item));
-        cityACTextView.addTextChangedListener(new LocationTextWatcher(cityACTextView));
 
         cityACTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new ShowEnterButton().execute();
+                new ShowEnterButton().execute(enterButton1);
             }
         });
 
@@ -109,12 +102,10 @@ public class OtherLocationFragment extends Fragment {
                 InputType.TYPE_NUMBER_FLAG_DECIMAL |
                 InputType.TYPE_NUMBER_FLAG_SIGNED);
 
-        latEditText.addTextChangedListener(new LocationTextWatcher(latEditText));
-
         latEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new ShowEnterButton().execute();
+                new ShowEnterButton().execute(enterButton2);
             }
         });
 
@@ -123,34 +114,40 @@ public class OtherLocationFragment extends Fragment {
                 InputType.TYPE_NUMBER_FLAG_DECIMAL |
                 InputType.TYPE_NUMBER_FLAG_SIGNED);
 
-        longEditText.addTextChangedListener(new LocationTextWatcher(longEditText));
-
         longEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new ShowEnterButton().execute();
+                new ShowEnterButton().execute(enterButton2);
             }
         });
 
-        enterButton = (Button)rootView.findViewById(R.id.enter_button);
-        enterButton.setVisibility(View.INVISIBLE);
-        enterButton.setOnClickListener(new View.OnClickListener() {
+        enterButton1 = (Button)rootView.findViewById(R.id.enter_button_1);
+        enterButton1.setVisibility(View.INVISIBLE);
+        enterButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(!(cityACTextView.getText().toString()).equals("")) {
                     new GetCoordinatesFromLocation().execute(cityACTextView.getText().toString());
                 } else {
-                    String lat = latEditText.getText().toString();
-                    String lon = longEditText.getText().toString();
+                    Toast.makeText(getActivity(), getResources().getString(R.string.no_loc_entered), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
-                    if(lat.equals("") && lon.equals("")) {
-                        Toast.makeText(getActivity(), NOTHING_ENTERED, Toast.LENGTH_LONG).show();
-                    } else if(lat.equals("") || lon.equals("")) {
-                        Toast.makeText(getActivity(), FILL_BOTH_FIELDS, Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getActivity(), lat + ", " + lon, Toast.LENGTH_LONG).show();
-                        launchMap(getOppositeCoordinates(Double.valueOf(lat), Double.valueOf(lon)));
-                    }
+        enterButton2 = (Button)rootView.findViewById(R.id.enter_button_2);
+        enterButton2.setVisibility(View.INVISIBLE);
+        enterButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String lat = latEditText.getText().toString();
+                String lon = longEditText.getText().toString();
+
+                if(lat.equals("") && lon.equals("")) {
+                    Toast.makeText(getActivity(), getResources().getString(R.string.no_coord_entered), Toast.LENGTH_LONG).show();
+                } else if(lat.equals("") || lon.equals("")) {
+                    Toast.makeText(getActivity(), getResources().getString(R.string.fill_both_fields), Toast.LENGTH_LONG).show();
+                } else {
+                    launchMap(Utils.getOppositeCoordinates(Double.valueOf(lat), Double.valueOf(lon)));
                 }
             }
         });
@@ -158,65 +155,9 @@ public class OtherLocationFragment extends Fragment {
         return rootView;
     }
 
-    // Show Enter button after delay
-    private class ShowEnterButton extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                Log.e(TAG, e.getMessage());
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void voidResult) {
-            if(enterButton.getVisibility() == View.INVISIBLE) {
-                enterButton.setVisibility(View.VISIBLE);
-            }
-        }
-    }
-
-    // TODO Handle timeout or Geocoder service unavailable...
-    private class GetCoordinatesFromLocation extends AsyncTask<String, Void, ArrayList<Double>> {
-
-        @Override
-        protected ArrayList<Double> doInBackground(String... location) {
-            ArrayList<Double> returnList = new ArrayList<Double>();
-            try {
-                Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
-                List<Address> addresses = geocoder.getFromLocationName(location[0], 1);
-                Address address = addresses.get(0); // TODO Handle bad input locations that render this an empty list...
-                returnList.add(address.getLatitude());
-                returnList.add(address.getLongitude());
-            } catch (IOException e) {
-                Log.e(TAG, e.getMessage());
-            }
-
-            return returnList;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Double> result) {
-            Toast.makeText(getActivity(), result.get(0) + ", " + result.get(1), Toast.LENGTH_LONG).show();
-            launchMap(getOppositeCoordinates(result.get(0), result.get(1)));
-        }
-    }
-
-    private double[] getOppositeCoordinates(double lat, double lon) {
-        double newLon = lon < 0.0 ? lon + 180.0 : lon - 180.0;
-        return new double[] { -1.0 * lat , truncate(newLon) };
-    }
-
-    // remove some precision to have consistent precision throughout app
-    private double truncate(double in) {
-        return Double.valueOf(new DecimalFormat("#.#######").format(in));
-    }
-
     private void launchMap(double[] args) {
-        Toast.makeText(getActivity(), "Opposite = " + args[0] + ", " + args[1], Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity(), "Opposite = " + args[0] + ", " + args[1], Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), getResources().getString(R.string.fetching_data), Toast.LENGTH_LONG).show();
         //TODO: launch map activity with args
     }
 
@@ -233,61 +174,60 @@ public class OtherLocationFragment extends Fragment {
         }
     }
 
-    /*
-     Return ArrayList of potential location matches to input string
-     */
-    private ArrayList<String> autocomplete(String input) {
-        ArrayList<String> resultList = null;
+    // Show Enter button after delay
+    private class ShowEnterButton extends AsyncTask<Button, Void, Button> {
 
-        HttpURLConnection conn = null;
-        StringBuilder jsonResults = new StringBuilder();
-        try {
-            StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
-            sb.append("?key=" + API_KEY);
-            sb.append("&input=" + URLEncoder.encode(input, "utf8"));
-
-            URL url = new URL(sb.toString());
-            conn = (HttpURLConnection) url.openConnection();
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
-
-            // Load the results into a StringBuilder
-            int read;
-            char[] buff = new char[1024];
-            while ((read = in.read(buff)) != -1) {
-                jsonResults.append(buff, 0, read);
+        @Override
+        protected Button doInBackground(Button... buttons) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                Log.e(TAG, e.getMessage());
             }
-        } catch (MalformedURLException e) {
-            Log.e(TAG, "Error processing Places API URL", e);
-            return resultList;
-        } catch (IOException e) {
-            Log.e(TAG, "Error connecting to Places API", e);
-            return resultList;
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
+            return buttons[0];
         }
 
-        try {
-            // Create a JSON object hierarchy from the results
-            JSONObject jsonObj = new JSONObject(jsonResults.toString());
-            JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
-
-            // Extract the Place descriptions from the results
-            resultList = new ArrayList<String>(predsJsonArray.length());
-            for (int i = 0; i < predsJsonArray.length(); i++) {
-                resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+        @Override
+        protected void onPostExecute(Button button) {
+            if(button.getVisibility() == View.INVISIBLE) {
+                button.setVisibility(View.VISIBLE);
             }
-        } catch (JSONException e) {
-            Log.e(TAG, "Cannot process JSON results", e);
         }
-
-        return resultList;
     }
 
-    /*
-     Provide interface to updating list of locations for AutoCompleteTextView
-     */
+    // TODO Handle timeout or Geocoder service unavailable...
+    private class GetCoordinatesFromLocation extends AsyncTask<String, Void, ArrayList<Double>> {
+
+        @Override
+        protected ArrayList<Double> doInBackground(String... location) {
+            ArrayList<Double> returnList = new ArrayList<Double>();
+            try {
+                Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                List<Address> addresses = geocoder.getFromLocationName(location[0], 1);
+                if(addresses.size() > 0) {
+                    Address address = addresses.get(0);
+                    returnList.add(address.getLatitude());
+                    returnList.add(address.getLongitude());
+                    return returnList;
+                }
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Double> result) {
+            if(result != null) {
+                launchMap(Utils.getOppositeCoordinates(result.get(0), result.get(1)));
+            } else {
+                Toast.makeText(getActivity(), getResources().getString(R.string.invalid_input), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    // Provide interface to updating list of locations for AutoCompleteTextView
     private class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
 
         private ArrayList<String> resultList;
@@ -339,27 +279,53 @@ public class OtherLocationFragment extends Fragment {
         }
     }
 
-    private class LocationTextWatcher implements TextWatcher {
+    // Return ArrayList of potential location matches to input string
+    private ArrayList<String> autocomplete(String input) {
+        ArrayList<String> resultList = null;
 
-        private EditText e;
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+        try {
+            StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+            sb.append("?key=" + API_KEY);
+            sb.append("&input=" + URLEncoder.encode(input, "utf8"));
 
-        public LocationTextWatcher(EditText e) {
-            this.e = e;
-        }
+            URL url = new URL(sb.toString());
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
 
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-            if(e.hashCode() == cityACTextView.hashCode()) {
-                //TODO empty other EditTexts text fields
-            } else {
-                //TODO empty cityACTextView text field
+            // Load the results into a StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Error processing Places API URL", e);
+            return resultList;
+        } catch (IOException e) {
+            Log.e(TAG, "Error connecting to Places API", e);
+            return resultList;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
             }
         }
 
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {}
+        try {
+            // Create a JSON object hierarchy from the results
+            JSONObject jsonObj = new JSONObject(jsonResults.toString());
+            JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
 
-        @Override
-        public void afterTextChanged(Editable editable) {}
+            // Extract the Place descriptions from the results
+            resultList = new ArrayList<String>(predsJsonArray.length());
+            for (int i = 0; i < predsJsonArray.length(); i++) {
+                resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Cannot process JSON results", e);
+        }
+
+        return resultList;
     }
 }
