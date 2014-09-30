509fc2523f660dcab7acd601574cd43d3666f029
dakotasanchez <dakotasanchez@outlook.com>
2014-09-29 18:05:12 -0700

    Change return statement in onCreateView

--- app/src/main/java/com/sanchez/geoopposite/MapFragment.java
+++ app/src/main/java/com/sanchez/geoopposite/MapFragment.java
@@ -38,7 +38,9 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+
+        return rootView;
     }
 }
