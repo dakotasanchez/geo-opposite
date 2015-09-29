3533a64d7bf0a7882c8fd191372c4bdc1174bbdc
dakotasanchez <dakota@dakotasanchez.com>
2015-09-28 18:08:21 -0700

    Fix number format exception

--- app/src/main/java/com/sanchez/geoopposite/Utils.java
+++ app/src/main/java/com/sanchez/geoopposite/Utils.java
@@ -5,9 +5,14 @@ Dakota Sanchez
 Summer 2014
  */
 
-import java.text.DecimalFormat;
+import android.util.Log;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class Utils {
+
+    private static NumberFormat formatter = NumberFormat.getInstance();
 
     public static double[] getOppositeCoordinates(double lat, double lon) {
         double newLon = lon < 0.0 ? lon + 180.0 : lon - 180.0;
@@ -16,7 +21,14 @@ public class Utils {
 
     // Remove some precision to have consistent precision throughout app
     public static double truncate(double in) {
-        return Double.valueOf(new DecimalFormat("#.#######").format(in));
+        formatter.setMinimumFractionDigits(7);
+        formatter.setMaximumFractionDigits(7);
+        double ret = in;
+        try {
+            ret = formatter.parse(formatter.format(in)).doubleValue();
+        } catch (ParseException e) {
+            Log.e("G.O. Utils", e.getMessage());
+        }
+        return ret;
     }
-
 }
